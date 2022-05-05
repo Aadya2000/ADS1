243860c83858226c4ff7082da16c165c4dd8b230
Aadya2000 <100848312+Aadya2000@users.noreply.github.com>
2022-05-05 17:36:06 +0530

    Add files via upload

---
+++ 220340320006/Q1/InserationSort.java
@@ -0,0 +1,42 @@
+class Inseration
+{
+void printarray(int arr[])
+{
+	int n=arr.length;
+	for(int i=0,i<n;i++)
+	{
+		System.out.print(arr[i]+"");
+	}
+	System.out.println(" ");
+}
+void inserationSort(int arr[])
+{
+	int n=arr.length;
+	for(int i=4;i>=0;i--)
+	{
+		int temp=arr[i];
+		int j=i-1;
+		while(j>=0&&arr[j]>temp)
+		{
+			arr[j+1]=arr[j];
+			printarray(arr);
+			j=j-1;
+		}
+		arr[j+1]=temp;
+	}
+	printarray(arr);
+	}
+	public static void main(String Args[])
+	{
+		Inseration i=new Inseration();
+		int A1[]={2,4,6,8,3};
+		i.inserationSort(A1);
+	}
+	
+	
+}
+
+
+
+
+}
